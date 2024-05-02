2a6fc3bb0b6210b2107a4c41bf0935e748869157
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-05-02 11:04:53 -0300

    [NO SCOPE]: reverting the emulated a error on the build process

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 			<h1>Best Shot</h1>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count?.test}
+					count is {count}
 				</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR

e973ed7817cf06bd99bcdba31e0480bbc54c0f7e
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-05-02 13:31:05 -0300

    with isolation...(2)

--- src/components/display/DisplayCopy3.test.tsx
+++ src/components/display/DisplayCopy3.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import { Display } from "./display";
+import { Display } from "./DisplayCopy3";
 
 describe("Display", () => {
 	it("should render the Display component", () => {

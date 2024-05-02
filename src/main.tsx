54b63848fb644f9f9515ae0e06defb579726af27
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-05-02 10:59:20 -0300

    [NO SCOPE]: adding prettier.rc and remove usused file

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import "./simple-script.ts";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );

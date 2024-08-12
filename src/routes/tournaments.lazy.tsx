fb256c5de3ef598f900cf3e6657571d7c89c9925
mario <mariobrusarosco@hotmail.com>
2024-08-12 19:02:09 -0300

    refactor: /tournaments screen

--- src/routes/tournaments.lazy.tsx
+++ src/routes/tournaments.lazy.tsx
@@ -1,13 +1,39 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useTournaments } from "../domains/tournament/hooks/use-tournaments";
+import { TournamentsList } from "../domains/tournament/components/tournaments-list";
 
-export const Route = createLazyFileRoute("/tournaments")({
-	component: Tournaments,
-});
+const TournamentsPage = () => {
+	const { data, isPending, error } = useTournaments();
 
-function Tournaments() {
+	if (error) {
+		return (
+			<div className="error tournaments-page-error">
+				We could not load all available tournaments
+			</div>
+		);
+	}
+
 	return (
-		<div className="p-2">
-			<h3>Tournaments</h3>
+		<div className="page">
+			<div>
+				<h2>Tournaments</h2>
+			</div>
+
+			{isPending ? (
+				<div>
+					<p>... LOADING TOURNAMENTS ...</p>
+				</div>
+			) : null}
+
+			{data ? (
+				<div>
+					<TournamentsList tournaments={data} />
+				</div>
+			) : null}
 		</div>
 	);
-}
+};
+
+export const Route = createLazyFileRoute("/tournaments")({
+	component: TournamentsPage,
+});

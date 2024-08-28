bd00a38ba90887689be762715031fdeab931dd00
mario <mariobrusarosco@hotmail.com>
2024-08-28 14:25:27 -0300

    fix: Build process

--- src/domains/dashboard/pages/index.tsx
+++ src/domains/dashboard/pages/index.tsx
@@ -1,25 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
-import { tempQueryFn } from "../../demo/utils";
-
-const Temp = () => {
-	const { data: testing } = useQuery({
-		queryKey: ["test"],
-		queryFn: tempQueryFn,
-	});
-
-	return <p>temp: {testing}</p>;
-};
-
 const DashboardPage = () => {
-	const { data: testing } = useQuery({
-		queryKey: ["test"],
-		queryFn: tempQueryFn,
-	});
-
 	return (
 		<div>
-			<h2>Dashboard {testing}</h2>
-			<Temp />
+			<h2>Dashboard </h2>
 		</div>
 	);
 };

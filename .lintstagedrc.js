0ab15c543cccb727d963b769d61f0e60ab418359
Mario Brusarosco <mariobrusarosco@hotmail.com>
2025-08-24 20:11:45 -0300

    [GENERAL FIXES]

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	// Run linting and formatting on TypeScript/JavaScript files
-	"**/*.{ts,tsx,js,jsx}": ["yarn format:fix", "yarn typecheck"],
+	"**/*.{ts,tsx,js,jsx}": ["yarn format:fix"],
 
 	// Run formatting on other supported files
 	"**/*.{json,md}": ["yarn format:fix"],

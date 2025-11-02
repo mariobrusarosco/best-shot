aae14189fc6f72c8b7b869e0e6ef6897d97d207a
Mario Brusarosco <mariobrusarosco@hotmail.com>
2025-11-02 16:26:42 -0300

    [fix] Route Issue

--- src/routes/index.route.tsx
+++ src/routes/index.route.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
+import { AppLoader } from "@/domains/global/components/app-loader";
 
 export const Route = createFileRoute("/")({
 	beforeLoad: () => {
 		throw redirect({ to: "/dashboard" });
 	},
+	component: () => <AppLoader />, // Show loader while redirecting
 });

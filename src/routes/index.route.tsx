44b2c28945b2dbad7700dc427f4dda110ed721ca
Mario Brusarosco <mariobrusarosco@hotmail.com>
2025-11-02 16:49:35 -0300

    [FIX] Loggin for debug purposes

--- src/routes/index.route.tsx
+++ src/routes/index.route.tsx
@@ -3,6 +3,7 @@ import { AppLoader } from "@/domains/global/components/app-loader";
 
 export const Route = createFileRoute("/")({
 	beforeLoad: () => {
+		console.log("redirecting to dashboard");
 		throw redirect({ to: "/dashboard" });
 	},
 	component: () => <AppLoader />, // Show loader while redirecting
